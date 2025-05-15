c085d8e59c18caca43f2993eb939a0fa2aad3d58
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-05-15 09:05:10 +0100

    start the project install axios and test it

---
+++ resources/js/Pages/HomeRoute.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <router-link to="/test">Test router</router-link>
+        <button @click.prevent="getValue()">Trigger button</button>
+        <p v-if="response">{{ response.data }}</p>
+    </div>
+</template>
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+const response = ref();
+const getValue = async () => {
+    try {
+        response.value = await axios.get("/api/test")
+    } catch (error) {
+        console.log(error)
+    }
+}
+</script>
---
+++ resources/js/Pages/TestRoute.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>this is router</p>
+    </div>
+</template>
