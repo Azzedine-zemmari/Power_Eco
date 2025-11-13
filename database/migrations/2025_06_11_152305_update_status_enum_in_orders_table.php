28645240e6a168395db69ecf8dbc9011e62b7df4
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-11-13 17:04:27 +0100

    Modifier : modifier migration update status

--- database/migrations/2025_06_11_152305_update_status_enum_in_orders_table.php
+++ database/migrations/2025_06_11_152305_update_status_enum_in_orders_table.php
@@ -7,36 +7,22 @@ use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::table('orders', function (Blueprint $table) {
-            // 1. Supprimer la contrainte de vérification
+        // Vérifie que la base n'est pas SQLite
+        if (Schema::getConnection()->getDriverName() === 'pgsql') {
             DB::statement("ALTER TABLE orders DROP CONSTRAINT IF EXISTS orders_status_check");
-            // 2. Mettre à jour les anciennes valeurs
             DB::table('orders')->where('status', 'order')->update(['status' => 'ordered']);
-            
-            // 3. Ajouter la nouvelle contrainte
             DB::statement("ALTER TABLE orders ADD CONSTRAINT orders_status_check CHECK (status IN ('ordered', 'paid', 'delivered', 'received'))");
-        });
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::table('orders', function (Blueprint $table) {
-            // 1. Supprimer la nouvelle contrainte
+        if (Schema::getConnection()->getDriverName() === 'pgsql') {
             DB::statement("ALTER TABLE orders DROP CONSTRAINT IF EXISTS orders_status_check");
-
-            // 2. Revenir aux anciennes valeurs
             DB::table('orders')->where('status', 'ordered')->update(['status' => 'order']);
-
-            // 3. Recréer l’ancienne contrainte
             DB::statement("ALTER TABLE orders ADD CONSTRAINT orders_status_check CHECK (status IN ('order', 'paid', 'delivered', 'received'))");
-        });
+        }
     }
 };
